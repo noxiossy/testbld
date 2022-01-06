0c83b8fb24b68e6833f714a3a77c05264b783459
noxiossy <72839483+noxiossy@users.noreply.github.com>
2022-01-06 14:51:10 +0700

    Update xr_efflensflare.h

--- xray/xrEngine/xr_efflensflare.h
+++ xray/xrEngine/xr_efflensflare.h
@@ -118,7 +118,7 @@ public:
 	void 				OnDeviceCreate	();         
 	void 				OnDeviceDestroy	();
 
-    shared_str			AppendDef		(CEnvironment& environment, CInifile* pIni, LPCSTR sect);
+    shared_str			AppendDef		(CEnvironment& environment, LPCSTR sect);
 
 	void				Invalidate		(){m_State=lfsNone;}
 };
